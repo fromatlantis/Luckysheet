1e4b413953bc007be53455c701adc551cfdcbd08
张傲 <zhangao30@jd.com>
2022-08-03 10:34:19 +0800

    fix: 兼容qiankun

--- src/utils/polyfill.js
+++ src/utils/polyfill.js
@@ -1,9 +1,9 @@
     /**
      * polyfill event in firefox
      */
-	function __firefox(){
+     function __firefox(){
         HTMLElement.prototype.__defineGetter__("runtimeStyle", __element_style);
-        window.constructor.prototype.__defineGetter__("event", __window_event);
+        // window.constructor.prototype.__defineGetter__("event", __window_event);
         Event.prototype.__defineGetter__("srcElement", __event_srcElement);
     }
 
@@ -20,27 +20,27 @@
     }
 
     function __window_event_constructor(){
-        if(document.all){
-            return window.event;
-        }
+        // if(document.all){
+        //     return window.event;
+        // }
 
-        var _caller = __window_event_constructor.caller;
+        // var _caller = __window_event_constructor.caller;
         
-        while(_caller != null){
-            var _argument = _caller.arguments[0];
+        // while(_caller != null){
+        //     var _argument = _caller.arguments[0];
 
-            if(_argument){
-                var _temp = _argument.constructor;
+        //     if(_argument){
+        //         var _temp = _argument.constructor;
                 
-                if(_temp.toString().indexOf("Event") != -1){
-                    return _argument;
-                }
-            }
+        //         if(_temp.toString().indexOf("Event") != -1){
+        //             return _argument;
+        //         }
+        //     }
 
-            _caller = _caller.caller;
-        }
+        //     _caller = _caller.caller;
+        // }
 
-        return null;
+        // return null;
     }
 
     export default __firefox;
